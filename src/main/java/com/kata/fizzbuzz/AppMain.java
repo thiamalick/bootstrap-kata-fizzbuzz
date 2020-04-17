999e3f6f86c8ca66e33ecc635f01d1cd2696af98
Kong To <newlight77@gmail.com>
2020-04-17 14:29:14 +0200

    initial commit with java for fizzbuzz

---
+++ src/main/java/com/kata/fizzbuzz/AppMain.java
@@ -0,0 +1,9 @@
+package com.kata.fizzbuzz;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/test/java/com/kata/fizzbuzz/AppMainTest.java
@@ -0,0 +1,14 @@
+package com.kata.fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppMainTest {
+
+    @Test
+    void myTest() {
+        assertEquals(true, true);
+    }
+
+}
